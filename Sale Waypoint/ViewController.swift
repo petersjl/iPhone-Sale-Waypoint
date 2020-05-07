62890a9edaabe44257d78e03b71837a45adb5c42
espinood <espinood@rose-hulman.edu>
2020-05-07 19:15:30 -0400

    Initial Commit

---
+++ Sale Waypoint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sale Waypoint
+//
+//  Created by CSSE Department on 5/7/20.
+//  Copyright © 2020 CSSE Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
